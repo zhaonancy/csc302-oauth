7fbb1ae6c8d4dde913df3d9eba29cec21589809f
zhaonancy <nancy.zhao@outlook.com>
2021-11-02 16:37:06 -0700

    Add uuid

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 const express = require('express')
 const app = express()
 const port = 3000
@@ -10,13 +11,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + "/client/login.html");
+})
+
 app.post('/login', (req, res) => {
   const username = req.body.username
   const password = req.body.password
+  const url = req.body.url
 
   if (username in Users && Users[username] == password) {
     const token = generateToken()
     res.send({ token: token })
+    res.redirect(url)
   } else {
     res.status(401).send()
   }
@@ -31,5 +38,5 @@ app.listen(port, () => {
 })
 
 function generateToken() {
-
+  return uuidv4()
 }
