2dfe6fbf0534a8b6962846d46b240872f269b278
zhaonancy <nancy.zhao@outlook.com>
2021-11-02 14:20:13 -0700

    Init login and validate routes

--- server/app.js
+++ server/app.js
@@ -2,10 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const Users = {
+  'test-user': 'test-password'
+}
+
 app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  if (username in Users && Users[username] == password) {
+    const token = generateToken()
+    res.send({ token: token })
+  } else {
+    res.status(401).send()
+  }
+})
+
+app.get('/validate', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Auth app listening at http://localhost:${port}`)
 })
+
+function generateToken() {
+
+}
